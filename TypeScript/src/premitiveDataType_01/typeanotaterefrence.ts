9aad1f2868334833b9cd6952dcb4128f6030b9f3
Mamoonkhan19 <hussainali197676@gmail.com>
2026-01-22 22:43:00 +0500

    type and refrece also updating the setting.html in student Form project

---
+++ TypeScript/src/premitiveDataType_01/typeanotaterefrence.ts
@@ -0,0 +1,16 @@
+// refrence the ts compiler give it a type number
+let a = 12; 
+let obj = {
+    name:"mamonkhan",
+}
+// anotation is when we define the datatype
+
+//  V:Dtype  = value; --> anotation     
+// we annotate and give data type to the variable so that the ts compiler do not give to it 
+
+let b: number = 12;
+let anotherObj : {name:string,age:number}={
+    name :"mamoonkhan",
+    age:15
+}
+
