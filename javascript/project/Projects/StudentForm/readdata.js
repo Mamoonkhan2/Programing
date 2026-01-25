9d60d312ec4bc6d52c1522ba63bc35651aea9880
Mamoonkhan19 <hussainali197676@gmail.com>
2026-01-26 00:15:05 +0500

    updating logic 12:14 1/26/2026

--- javascript/project/Projects/StudentForm/readdata.js
+++ javascript/project/Projects/StudentForm/readdata.js
@@ -1,48 +1,90 @@
-// let fs = require('fs')
+const { log } = require('console');
+let fs = require('fs')
+const { json } = require('stream/consumers')
 // const customer = {
 //     name : 'mamoonkhan',
 //     age  : 20,
-//     college : 'state group of college',
-//     degree  : "computersScienceds"
+//     college : 'swtate group of college',
+//     degree  : "23432432424"
 // }
 
-// // synchrounesly the file will be read
-// // let synchPassObj = JSON.stringify(customer,null,2);fs.writeFileSync('./fukingData.json',jsonObj);
+// synchrounesly the file will be read
+// let synchPassObj = JSON.stringify(customer,null,2);
+// fs.writeFileSync('./fukingData.json',synchPassObj);
 
-// // asyncrounesly writing into the file
-// let asyncPassObj = JSON.stringify(customer,null,4); fs.writeFile('./fuckingData.json',asyncPassObj,err=>console.log('error while loading file is ',err));
+// // // asyncrounesly writing into the file
+// let asyncPassObj = JSON.stringify(customer,null,4); 
+// fs.writeFile('./fukingData.json',asyncPassObj,err=>{
+//      if (err) {
+//         console.log('Error writing file', err)
+//     } else {
+//         console.log('Successfully wrote file')
+//     }
+// });
+
+function SyncWriter(obj,path) {
+    let data = JSON.stringify(obj,null,4);
+    fs.writeFileSync(path,data)
+    console.log("succsessfully wriet data into");
+    
+}
+SyncWriter({name:"harron",age:23,sex:'Male',studying:"Law"},'./fukingData.json')
+
+function AsyncWritingIntoFile(obj,path) {
+    let data = JSON.stringify(obj,null,4);
+    fs.writeFile(path,data,err => {
+        if(err){
+            log('error while writing is ',err)
+        }
+        else{
+            log("write succsesfully")
+        }
+    }
+    )
+}
+
+// AsyncWritingIntoFile({name:"aginChangeing.json"},'./fukingData.json');
+// const { log } = require("console");
+// let fs = require("fs");
+let PATH = './data.json'
+let ENCODE = 'utf-8'
+
+// // let data = fs.readFileSync('./fukingData.json',encode);
+// // let JSOBJECTDATA = JSON.parse(data)
+// // console.log(JSOBJECTDATA);
 
 
-let fs = require("fs");
-let path = './fuckingData.json'
-let encode = 'utf-8'
-
-// let data = fs.readFileSync('./fukingData.json',encode);
-// let JSOBJECTDATA = JSON.parse(data)
-// console.log(JSOBJECTDATA);
-
-
-function err(cb) {
+function acyncReader(path,encode,cb) {
        fs.readFile(path,encode,(error,succs)=>{
         if(error)
         {
-            return cb && cb(err);
+            return cb && cb(error);
         }
 
         try {
             let customer = JSON.parse(succs);
             return cb && cb(null,customer);
-        } catch (error) {
+        } catch (err) {
             return cb && cb(err);
         }
 
        })
 }
 
-err((err,obj)=>{
-    if(err){
-        console.log("error is ",err);
-        return
-    }
-    
-})
+// acyncReader(PATH,ENCODE,function callback(err,obj){
+//     if(err){
+//         console.log("error is ",err);
+//         return -1;
+//     }
+//     else{
+//         log(obj)
+//     }
+// })
+
+// function SynchReader(path) {
+//     const jsonString = fs.readFileSync("./data.json");
+//     const customer = JSON.parse(jsonString);
+//     return customer;
+// }
+// let data = SynchReader(PATH);
+// log(data)
