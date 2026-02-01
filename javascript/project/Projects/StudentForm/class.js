ba13355d9e76870afa7388a2a0255b08719b4368
Mamoonkhan19 <hussainali197676@gmail.com>
2026-02-01 23:51:39 +0500

    updating the studentform project and also adding the class more on that inshallah will make a pop up and many more pakeges over there

--- javascript/project/Projects/StudentForm/class.js
+++ javascript/project/Projects/StudentForm/class.js
@@ -1,9 +1,31 @@
-export default  class users {
-    constructor(name,bgc) {
-        this.name = name;
-        this.bgc  = bgc ;
+export class std {
+    constructor(remainingTime) {
+        this.remainingTime = remainingTime;
     }
-    gretting(){
-        return `your name is ${this.name}`
-    }
+   timoutFun(){
+        let div = document.createElement('div');
+        let h3 = document.createElement('h3');
+        // setting atttribute
+        div.setAttribute('class',"timeout")
+        h3.setAttribute("id","sec-dis")
+        div.appendChild(h3);            
+        
+        let secoundLeft = Math.floor(this.remainingTime/1000); // convert like 10000/1000 = 10sec
+        
+        
+        let interval = setInterval(() =>
+            {
+                if(secoundLeft<=0){// condition when the time is out 
+                    clearInterval(interval);
+                    h3.innerHTML = "Time's up!";
+                    return;
+
+                }
+                h3.innerHTML=`${secoundLeft} Secound left `;
+                secoundLeft--;
+            }
+            ,1000)
+
+        return div;
+   }
 }
