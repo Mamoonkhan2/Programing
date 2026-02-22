8deaeb800c2e5787a7d4ce9a6b9c413d111ae950
Mamoonkhan19 <hussainali197676@gmail.com>
2026-02-23 00:15:28 +0500

    updating in react

--- react/04bgchanger/src/App.jsx
+++ react/04bgchanger/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [color,setcolor] = useState('olive');
   return (
     <>
-     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+     <div className="w-full h-screen duration-900" style={{backgroundColor:color}}>
           
           <div className="fixed flex flex-wrap justify-center  bottom-12 inset-x-0 px-2">
             <div className="flex flex-wrap gap-3 px-2 py-2 rounded-xl shadow-lg justify-center bg-white">
